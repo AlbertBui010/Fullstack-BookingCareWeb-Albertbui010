660646903582e87fec5b8654034088841e3d7bcb
AlbertBui010 <buiquangquy12823@gmail.com>
2024-08-04 15:48:32 +0700

    #71 Tạo Menu Phân Quyền Người Dùng Khi Đăng Nhập

--- Reactjs/src/containers/Header/menuApp.js
+++ Reactjs/src/containers/Header/menuApp.js
@@ -4,14 +4,17 @@ export const adminMenu = [
 		name: 'menu.admin.manage-user',
 		menus: [
 			{
+				// user manage
 				name: 'menu.admin.crud',
 				link: '/system/user-manage',
 			},
 			{
+				// crud user
 				name: 'menu.admin.crud-redux',
 				link: '/system/user-redux',
 			},
 			{
+				// manage-doctor
 				name: 'menu.admin.manage-doctor',
 				link: '/system/manage-doctor',
 				// subMenus: [
@@ -19,9 +22,14 @@ export const adminMenu = [
 				// 	{ name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
 				// ],
 			},
+			// { // manage-admin
+			// 	name: 'menu.admin.manage-admin',
+			// 	link: '/system/user-admin',
+			// },
 			{
-				name: 'menu.admin.manage-admin',
-				link: '/system/user-admin',
+				// Manage schedule doctor
+				name: 'menu.doctor.manage-schedule',
+				link: '/doctor/manage-schedule',
 			},
 		],
 	},
@@ -52,6 +60,19 @@ export const adminMenu = [
 			{
 				name: 'menu.admin.manage-handbook',
 				link: '/system/manage-handbook',
+			},
+		],
+	},
+];
+
+export const doctorMenu = [
+	{
+		name: 'menu.admin.manage-user',
+		menus: [
+			{
+				// Manage schedule doctor
+				name: 'menu.doctor.manage-schedule',
+				link: '/doctor/manage-schedule',
 			},
 		],
 	},
